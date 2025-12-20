576572c2f161d5dad9074db87f280b9ea6ebc094
freelanceradnan <reactorbro722@gmail.com>
2025-12-20 22:53:58 +0600

    middleware implement in function

--- src/middleware/OurMiddleWare.js
+++ src/middleware/OurMiddleWare.js
@@ -1,29 +1,35 @@
 export const ourMiddleWare=(store)=>(next)=>async(action)=>{
- console.log(JSON.stringify(store.getState()),"store State")
- console.log(action,"action")
+//  console.log(JSON.stringify(store.getState()),"store State")
+//  console.log(action,"action")
  if(action==='fake-post-dispatch'){
-    store.dispatch({type:"post/fetchStarted"})
+    fetchPost(store.dispatch)
+    return
+ }
+ if(action=='fake-user-dispatch'){
+    fetchUserPost(store.dispatch)
+    return
+ }
+next(action)
+}
+const fetchPost=async(dispatch)=>{
+    dispatch({type:"post/fetchStarted"})
     try {
-        const res=await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
+const res=await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
   const data=await res.json()
- store.dispatch({type:"post/fetchSucceeded",payload:data})
+ dispatch({type:"post/fetchSucceeded",payload:data})
  
     } catch (error) {
         store.dispatch({type:"post/fetchFailed",payload:error.message})
     }
-    return
- }
- if(action=='fake-user-dispatch'){
-    store.dispatch({type:"user/fetchStarted"});
+}
+const fetchUserPost=async(dispatch)=>{
+    dispatch({type:"user/fetchStarted"});
     try {
         const res=await fetch('https://jsonplaceholder.typicode.com/users?_limit=5')
         const data=await res.json()
-        store.dispatch({type:"user/fetchSucceeded",payload:data})
+        dispatch({type:"user/fetchSucceeded",payload:data})
         
     } catch (error) {
-        store.dispatch({type:"user/fetchFaild",payload:error.message})
+        dispatch({type:"user/fetchFaild",payload:error.message})
     }
-    
- }
-next(action)
 }
